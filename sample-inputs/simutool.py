f56c32fb4d12cd3973d7b5f32eebc47f76f0166d
ba2ag9 <nasr.kasrin@uni-bamberg.de>
2019-07-19 14:53:42 +0200

    added sample inputs

---
+++ sample-inputs/simutool.py
@@ -0,0 +1,17 @@
+  # Low-level details of the domain model. are specific to the target domain, and hence will contain specific information about manufacturing processes, resources, and so on. This information can be changed according to the specifications of the end-users 
+# Created 2019-07-12 16:49:32.430000 
+
+# Dict storing class info: 
+classes = [{'ToolDevelopment': {'admin': '',
+                      'description': 'tbd',
+                      'html_info': '',
+                      'identifier': 'http://example.org/tbox/tooldevelopment',
+                      'label': 'TBox',
+                      'ontology_level': 'simutool',
+                      'optional_property': [],
+                      'pl': 'Tool Development Activities',
+                      'required_property': [],
+                      'sing': 'Tool Development',
+                      'subclass_of': 'Activity',
+                      'title': 'ToolDevelopment',
+                      'version': 'v3.1'}}]
